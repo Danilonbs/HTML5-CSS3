2032f81c3b56091d3ee84aaf372e374819c996a4
Danilonbs <112011887+Danilonbs@users.noreply.github.com>
2022-09-29 18:00:30 -0300

    Initial commit

---
+++ Ex008/novo.js
@@ -0,0 +1,8 @@
+let altura = document.getElementById('faltura')
+let idade = document.getElementById('fidade')
+
+function soma(n) {
+    n = (altura.value * idade.value)
+    res.innerHTML =`a idade e ${n}`
+}
+
